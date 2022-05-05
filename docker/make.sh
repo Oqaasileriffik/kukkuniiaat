ac35ae88fda3d9c6850051dec0bdc832127eaf11
Tino Didriksen <mail@tinodidriksen.com>
2022-05-05 10:57:46 +0000

    Build nonce

--- docker/make.sh
+++ docker/make.sh
@@ -3,4 +3,5 @@ set -e
 export DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain PROGRESS_NO_TRUNC=1
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-docker build --squash -t oqaasileriffik/kukkuniiaat-backend .
+NONCE=$(date '+%s')
+docker build --build-arg "CACHE_NONCE=$NONCE" --squash -t oqaasileriffik/kukkuniiaat-backend .
---
+++ docker/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -dtp 12400:12400 --restart always --name kukkuniiaat-backend oqaasileriffik/kukkuniiaat-backend
